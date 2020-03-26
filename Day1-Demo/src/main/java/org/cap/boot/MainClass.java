ad67ff59af1e4ca85c044cc9a702073e484f4889
Hemanth <Hemanth@Hemanth-PC>
2020-03-26 15:21:59 +0530

    My first commit

---
+++ Day1-Demo/src/main/java/org/cap/boot/MainClass.java
@@ -0,0 +1,14 @@
+package org.cap.boot;
+
+import org.cap.model.Sales;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Sales obj=new Sales();
+		obj.print();
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Day1-Demo/src/main/java/org/cap/model/Sales.java
@@ -0,0 +1,13 @@
+package org.cap.model;
+
+public class Sales {
+	
+	int[] arr = {23,45,67,-90,2};
+	
+	public void print() {
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+			
+	}
+
+}
