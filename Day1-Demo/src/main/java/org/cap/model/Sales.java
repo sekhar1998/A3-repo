d8d47942272a35bf451931bc34d6f75014395eea
Hemanth <Hemanth@Hemanth-PC>
2020-03-27 16:31:21 +0530

    sales enhanced loop updated

--- Day1-Demo/src/main/java/org/cap/model/Sales.java
+++ Day1-Demo/src/main/java/org/cap/model/Sales.java
@@ -5,9 +5,18 @@ public class Sales {
 	int[] arr = {23,45,67,-90,2};
 	
 	public void print() {
+		int sum=0;
 		for(int i=0;i<arr.length;i++)
+		{
+			sum+=arr[i];
 			System.out.println(arr[i]);
+		}
+			System.out.println("Summation:" + sum);
 			
+	}
+	public void display()
+	{
+		System.out.println("Display sales details here!");
 	}
 
 }
